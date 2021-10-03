e8469c4d0e009e1ad9008e048ad5aee8078ee5d9
Hanul <hanul@hanul.me>
2021-10-03 19:38:45 +0900

    dev cakepot

---
+++ contracts/interfaces/ICakeStaker.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface ICakeStaker {
+    function enterStaking(uint256 amount) external;
+    function leaveStaking(uint256 amount) external;
+    function pendingCake(uint256 pid, address user) external view returns (uint256);
+    function userInfo(uint256 pid, address user) view external returns(uint256 amount, uint256 rewardDebt);
+}
---
+++ scripts/deployCakePot.ts
@@ -0,0 +1,16 @@
+import hardhat from "hardhat";
+
+async function main() {
+  console.log("deploy start");
+
+  const CakePot = await hardhat.ethers.getContractFactory("CakePot");
+  const cakePot = await CakePot.deploy();
+  console.log(`CakePot address: ${cakePot.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
